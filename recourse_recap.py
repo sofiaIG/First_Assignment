b804a7dfc4e92b299efa68d7de2a866fb4394a10
user <user@users-MacBook-Pro.local>
2022-02-21 22:24:29 +0000

    first homework assignment

---
+++ recourse_recap.py
@@ -0,0 +1,7 @@
+integet = 1
+string = "random"
+float_number = 5.1
+string_integer = str(integet)
+capitalize = string.capitalize()
+float_number_int = int(float_number)
+sum_of_variables = float_number_int +integet
