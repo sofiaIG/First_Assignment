beba54ba52ffd894259585cf79ed79178a1a7cfd
user <user@users-MacBook-Pro.local>
2022-02-22 10:15:12 +0000

    fixed a spelling mistake

--- recourse_recap.py
+++ recourse_recap.py
@@ -1,7 +1,7 @@
-integet = 1
+integer = 1
 string = "random"
 float_number = 5.1
-string_integer = str(integet)
+string_integer = str(integer)
 capitalize = string.capitalize()
 float_number_int = int(float_number)
-sum_of_variables = float_number_int +integet
+sum_of_variables = float_number_int +integer
